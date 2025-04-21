4ac1cf09951955fce7bf421ec4fae1881c0b39ed
w-old-man <at394996767@163.com>
2025-04-21 10:07:01 +0800

    fix: 修复版本冲突

--- es/components/upload/upload.js
+++ es/components/upload/upload.js
@@ -1,8 +1,7 @@
 import { jsx as o } from "react/jsx-runtime";
-import { Upload as s } from "antd";
-import a from "./index.module.scss.js";
-console.log(a);
-const e = () => /* @__PURE__ */ o(s, { className: "sykj-container", children: /* @__PURE__ */ o("div", { className: "sykj-upload", children: "asdasd" }) });
+import { Upload as a } from "antd";
+import s from "./index.module.scss.js";
+const e = () => /* @__PURE__ */ o(a, { className: s["sykj-container"], children: /* @__PURE__ */ o("div", { className: s["sykj-upload"], children: "asdasd" }) });
 export {
   e as Upload
 };
